65eb95cc3c2680877af236702bd13eaa4d15485b
Wertolet9999 <wertolet9999@gmail.com>
2023-01-19 19:52:50 +0200

    fix firebase

--- src/js/auth/firebase-conf/index.js
+++ src/js/auth/firebase-conf/index.js
@@ -1,12 +1,11 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyDLRiNB6Ng2xMkVzqZrEKTQQ81dcxsOiZY',
-  authDomain: 'coctailteam-d2c89.firebaseapp.com',
-  databaseURL:
-    'https://coctailteam-d2c89-default-rtdb.europe-west1.firebasedatabase.app',
-  projectId: 'coctailteam-d2c89',
-  storageBucket: 'coctailteam-d2c89.appspot.com',
-  messagingSenderId: '80605757667',
-  appId: '1:80605757667:web:fc461ced9d4dad937045d6',
+  apiKey: "AIzaSyBIuUvjb6xAQOfYqwRfjRzcTRQgvvCJ1o0",
+  authDomain: "coctails-bar-project.firebaseapp.com",
+  databaseURL: "https://coctails-bar-project-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "coctails-bar-project",
+  storageBucket: "coctails-bar-project.appspot.com",
+  messagingSenderId: "776647847470",
+  appId: "1:776647847470:web:69b3f288ac852fcd68b9bf",
 };
 
 // Initialize Firebase
